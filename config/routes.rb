a334e06eb262059c77a06bf20c1fa5bd5dd7ad37
camli <camli1803>
2021-12-10 23:37:18 +0700

    add homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'static_page/home'
   devise_for :users
   resources :subjects
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'static_page#home'
 end
