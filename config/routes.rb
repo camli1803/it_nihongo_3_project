557a17722858b0433508efd936ed060927d7e66f
camli <camli1803>
2021-12-11 11:37:12 +0700

    add profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  root 'static_page#home' 
   get 'static_page/home'
   devise_for :users
   resources :subjects
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'static_page#home'
 end
