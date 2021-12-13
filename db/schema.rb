22c2e1f5f288ad849fca58cd5845ee14456ddf02
camli <camli1803>
2021-12-13 21:00:13 +0700

    add name age

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_10_161758) do
+ActiveRecord::Schema.define(version: 2021_12_11_101216) do
 
   create_table "subjects", force: :cascade do |t|
     t.string "subject_name"
@@ -27,7 +27,10 @@ ActiveRecord::Schema.define(version: 2021_12_10_161758) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.integer "age"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
